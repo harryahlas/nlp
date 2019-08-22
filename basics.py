f65f4c31c375535efd5e35e9199bc496211330e4
harryahlas <42125399+harryahlas@users.noreply.github.com>
2019-08-21 22:28:59 -0700

    Update basics.py

--- basics.py
+++ basics.py
@@ -2,6 +2,8 @@
 """
 Created on Wed Aug 21 20:35:54 2019
 
+https://www.dataquest.io/blog/tutorial-text-classification-in-python-using-spacy/
+
 @author: Anyone
 """
 
@@ -17,3 +19,17 @@ print('Number of stop words: %d' % len(spacy_stopwords))
 
 #Printing first ten stop words:
 print('First ten stop words: %s' % list(spacy_stopwords)[:20])
+
+#from spacy.lang.en.stop_words import STOP_WORDS
+
+#Implementation of stop words:
+filtered_sent=[]
+
+#  "nlp" Object is used to create documents with linguistic annotations.
+doc = nlp(text)
+
+# filtering stop words
+for word in doc:
+    if word.is_stop==False:
+        filtered_sent.append(word)
+print("Filtered Sentence:",filtered_sent)
