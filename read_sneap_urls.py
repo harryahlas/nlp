b8cbbd2e2d8b070ff98b4a7dfc1891eb51da20bb
harryahlas <42125399+harryahlas@users.noreply.github.com>
2019-08-23 12:41:23 -0700

    Update read_sneap_urls.py
    find_text

--- read_sneap_urls.py
+++ read_sneap_urls.py
@@ -6,6 +6,7 @@ Created on Thu Aug 22 22:25:07 2019
 """
 
 import requests
+import re
 
 link = "http://www.ultimatemetal.com/forum/threads/tube-screamer-faq-version-1.346068/page-2"
 link = "http://www.ultimatemetal.com/forum/forums/andy-sneap-backline/page-101"
@@ -17,16 +18,31 @@ filehandle.write(f.text)
 filehandle.close()
 
 
+# Function to search for text between 2 markers
+def find_text(text, start_marker, end_marker):
+    result = re.search(start_marker + "(.*)" + end_marker, text)
+    output = print(result.group(1))
+    return output
+
+
 # section_name = Backline, section_number = 1
 section_name = Backline
 section_number = 1
 
-def find_text(text, start_marker, end_marker):
-    
+
+start_marker = 'data-last="'
+end_marker = '"'
+
+find_text('xxx" data-last="jjjjsd" hellow', 'data-last="', '"' )
     
 # Look up value for last page in data-last=
 data_last_page = data-last="1022"
 ##  This will become loop
+
+text = 'xxx" data-last="jjjjsd" hellow'
+s = 'xxx" data-last="jjjjsd" hellow'
+result = re.search('data-last="(.*)"', s)
+print(result.group(1))
 
 # Look up titles/links on current page
 # Start Marker for link
