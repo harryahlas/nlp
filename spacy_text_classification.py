ce20bf639e0b36060fdb59539b67fdd63fe7c05f
harryahlas <42125399+harryahlas@users.noreply.github.com>
2019-08-22 20:46:05 -0700

    Update spacy_text_classification.py

--- spacy_text_classification.py
+++ spacy_text_classification.py
@@ -134,7 +134,6 @@ mango.vector.shape
 print(mango.vector)
 
 
-
 # Text Classification
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
@@ -153,8 +152,57 @@ from spacy.lang.en import English
 # Create our list of punctuation marks
 punctuations = string.punctuation
 
-nlp = spacy.load('en')
+#nlp = spacy.load('en')
+nlp = spacy.load('en_core_web_sm') # can use this instead of above
 stop_words = spacy.lang.en.stop_words.STOP_WORDS
 
 # Load English tokenizer, tagger, parser, NER and word vectors
 parser = English()
+
+# Tokenize, strip punctuation and stop words, lemmatize, lower case
+
+# Creating our tokenizer function
+def spacy_tokenizer(sentence):
+    # Creating our token object, which is used to create documents with linguistic annotations.
+    mytokens = parser(sentence)
+
+    # Lemmatizing each token and converting each token into lowercase
+    mytokens = [ word.lemma_.lower().strip() if word.lemma_ != "-PRON-" else word.lower_ for word in mytokens ]
+
+    # Removing stop words
+    mytokens = [ word for word in mytokens if word not in stop_words and word not in punctuations ]
+
+    # return preprocessed list of tokens
+    return mytokens
+
+# To further clean our text data, we’ll also want to create a custom transformer for removing initial and end spaces and converting text into lower case
+# Custom transformer using spaCy
+class predictors(TransformerMixin):
+    def transform(self, X, **transform_params):
+        # Cleaning Text
+        return [clean_text(text) for text in X]
+
+    def fit(self, X, y=None, **fit_params):
+        return self
+
+    def get_params(self, deep=True):
+        return {}
+
+# Basic function to clean the text
+def clean_text(text):
+    # Removing spaces and converting text into lowercase
+    return text.strip().lower()
+
+# Vectorization Feature Engineering (TF-IDF)
+# Set ngrams for tokenizer
+bow_vector = CountVectorizer(tokenizer = spacy_tokenizer, ngram_range=(1,1))
+#TF-IDF
+tfidf_vector = TfidfVectorizer(tokenizer = spacy_tokenizer)
+
+# Split into train and test
+from sklearn.model_selection import train_test_split
+
+X = df_amazon['verified_reviews'] # the features we want to analyze
+ylabels = df_amazon['feedback'] # the labels, or answers, we want to test against
+
+X_train, X_test, y_train, y_test = train_test_split(X, ylabels, test_size=0.3)
