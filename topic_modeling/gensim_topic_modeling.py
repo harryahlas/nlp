e1e5dadb67e37f9b80b8823fe7774bf45e1568b2
harryahlas <42125399+harryahlas@users.noreply.github.com>
2020-04-11 15:56:47 -0700

    Update gensim_topic_modeling.py

--- topic_modeling/gensim_topic_modeling.py
+++ topic_modeling/gensim_topic_modeling.py
@@ -51,3 +51,93 @@ stop_words = [word for word in stop_words if word not in remove_list]
 df = pd.read_json('https://raw.githubusercontent.com/selva86/datasets/master/newsgroups.json')
 print(df.target_names.unique())
 df.head()
+
+# Convert to list
+data = df.content.values.tolist()
+
+# Remove Emails
+data = [re.sub('\S*@\S*\s?', '', sent) for sent in data]
+
+# Remove new line characters
+data = [re.sub('\s+', ' ', sent) for sent in data]
+
+# Remove distracting single quotes
+data = [re.sub("\'", "", sent) for sent in data]
+
+pprint(data[:1])
+
+# tokenize
+def sent_to_words(sentences):
+    for sentence in sentences:
+        yield(gensim.utils.simple_preprocess(str(sentence), deacc=True))  # deacc=True removes punctuations
+
+data_words = list(sent_to_words(data))
+
+print(data_words[:1])
+
+
+# Build the bigram and trigram models
+# The two important arguments to Phrases are min_count and threshold.
+# The higher the values of these param, the harder it is for words to be combined to bigrams.
+bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100) # higher threshold fewer phrases.
+trigram = gensim.models.Phrases(bigram[data_words], threshold=100)  
+
+# Faster way to get a sentence clubbed as a trigram/bigram
+bigram_mod = gensim.models.phrases.Phraser(bigram)
+trigram_mod = gensim.models.phrases.Phraser(trigram)
+
+# See trigram example
+print(trigram_mod[bigram_mod[data_words[0]]])
+
+# Define functions for stopwords, bigrams, trigrams and lemmatization
+def remove_stopwords(texts):
+    return [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+
+def make_bigrams(texts):
+    return [bigram_mod[doc] for doc in texts]
+
+def make_trigrams(texts):
+    return [trigram_mod[bigram_mod[doc]] for doc in texts]
+
+def lemmatization(texts, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
+    """https://spacy.io/api/annotation"""
+    texts_out = []
+    for sent in texts:
+        doc = nlp(" ".join(sent)) 
+        texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
+    return texts_out
+
+
+# Remove Stop Words
+data_words_nostops = remove_stopwords(data_words)
+
+# Form Bigrams
+data_words_bigrams = make_bigrams(data_words_nostops)
+
+# Initialize spacy 'en' model, keeping only tagger component (for efficiency)
+# python3 -m spacy download en
+nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
+
+# Do lemmatization keeping only noun, adj, vb, adv
+data_lemmatized = lemmatization(data_words_bigrams, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV'])
+
+print(data_lemmatized[:1])
+
+# The two main inputs to the LDA topic model are the dictionary(id2word) and the corpus.
+# Create Dictionary
+id2word = corpora.Dictionary(data_lemmatized)
+
+# Create Corpus
+texts = data_lemmatized
+
+# Term Document Frequency
+corpus = [id2word.doc2bow(text) for text in texts]
+
+# View. Gensim creates a unique id for each word in the document. 
+# The produced corpus shown above is a mapping of (word_id, word_frequency).
+# For example, (0, 1) above implies, word id 0 occurs once in the first document. 
+# Likewise, word id 1 occurs twice and so on.
+print(corpus[:1])
+
+# If you want to see what word a given id corresponds to, pass the id as a key to the dictionary.
+id2word[0]
